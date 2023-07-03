53e7f8951f7cff05ac6ac733e2cba223e40e982f
Suvendu Mondal <suvendumandal742@gmail.com>
2023-07-03 07:32:22 +0530

    add

--- JS/main.js
+++ JS/main.js
@@ -17,3 +17,25 @@ navBar.forEach(function(a){
         navCollapse.classList.remove("show");
     })
 })
+
+
+//Good morning, 
+let myDate = new Date();
+let hrs = myDate.getHours();
+let greet;
+if(hrs>=0 && hrs<12){
+    greet = 'Good Morning';
+}
+else if(hrs == 12){
+    greet = 'Good Noon';
+}
+else if(hrs>=12 && hrs<=17){
+    greet = 'Good Afternoon';
+}
+else if(hrs>=17 && hrs<=20){
+    greet = 'Good Evening';
+}
+else{
+    greet = 'Good Night';
+}
+document.getElementById('time').innerHTML ='<b>' + greet + '</b>';

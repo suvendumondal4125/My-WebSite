608bf2d33b841e9f862c0ea29941d613a225d582
Suvendu Mondal <suvendumandal742@gmail.com>
2023-07-12 17:58:31 +0530

    commit

--- JS/main.js
+++ JS/main.js
@@ -29,7 +29,7 @@ if(hrs>=0 && hrs<12){
 else if(hrs == 12){
     greet = 'Good Noon';
 }
-else if(hrs>=12 && hrs<=17){
+else if(hrs>=12 && hrs<17){
     greet = 'Good Afternoon';
 }
 else if(hrs>=17 && hrs<20){
